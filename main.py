c75bb5454330d88048ab35b5c21ffc82b30553dc
Minh Doan <daywednes@gmail.com>
2021-01-19 10:35:02 +0700

    init

---
+++ main.py
@@ -0,0 +1,30 @@
+# import todolist_pb2 as TodoList
+import ai_response_pb2 as AIResponse
+
+response = AIResponse.YoloResponse()
+response.screen_width = 3;
+
+print(response);
+
+# my_list = TodoList.TodoList()
+# my_list.owner_id = 1234
+# my_list.owner_name = "Tim"
+#
+# first_item = my_list.todos.add()
+# first_item.state = TodoList.TaskState.Value("TASK_DONE")
+# first_item.task = "Test ProtoBuf for Python"
+# first_item.due_date = "31.10.2019"
+#
+#
+# # ...
+#
+# with open("./serializedFile", "wb") as fd:
+#     fd.write(my_list.SerializeToString())
+# print(my_list.SerializeToString())
+#
+#
+# my_list = TodoList.TodoList()
+# with open("./serializedFile", "rb") as fd:
+#     my_list.ParseFromString(fd.read())
+#
+# print(my_list)
